18f074a0f9d9c4e50d2a907dd70c5965286b6168
Chumbinho1 <vitorpereira041019@gmail.com>
2024-10-26 13:56:58 -0300

    create update function

--- app/Http/Controllers/TaskModule/TaskController.php
+++ app/Http/Controllers/TaskModule/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\TaskModule;
 use App\Exceptions\NotFoundException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Task\TaskStoreRequest;
+use App\Http\Requests\Task\TaskUpdateRequest;
 use App\Services\Task\TaskService;
 use Illuminate\Http\JsonResponse;
 
@@ -24,6 +25,22 @@ class TaskController extends Controller
             $this->taskService->create($request->validated());
 
             return created('Task');
+        } catch (NotFoundException $e) {
+            return notFound($e->getMessage());
+        } catch (\Exception $e) {
+            return internalServerError($e);
+        }
+    }
+
+    public function update(
+        string $id,
+        TaskUpdateRequest $request
+    ): JsonResponse
+    {
+        try {
+            $this->taskService->update($id,$request->validated());
+
+            return ok(['message' => 'Task updated successfully!']);
         } catch (NotFoundException $e) {
             return notFound($e->getMessage());
         } catch (\Exception $e) {
--- app/Services/Task/TaskService.php
+++ app/Services/Task/TaskService.php
@@ -2,7 +2,9 @@
 
 namespace App\Services\Task;
 
+use App\Exceptions\NotFoundException;
 use App\Models\Task;
+use Illuminate\Support\Facades\DB;
 
 class TaskService
 {
@@ -17,9 +19,32 @@ class TaskService
         array $data
     ): Task
     {
-        $data['user_id'] = auth()->id();
-        $data['task_status_id'] = $this->taskStatusService->getTaskStatusBySlug('to-do')->id;
+        return DB::transaction(function () use ($data) {
+            $data['user_id'] = auth()->id();
+            $data['task_status_id'] = $this->taskStatusService->getTaskStatusBySlug('to-do')->id;
 
-        return $this->taskModel->create(convertKeysToSnakeCase($data));
+            return $this->taskModel->create(convertKeysToSnakeCase($data));
+        });
+    }
+
+    public function update(
+        string $id,
+        array  $data
+    ): Task
+    {
+        return DB::transaction(function () use ($id, $data) {
+            $task = $this->getTaskById($id);
+            $task->update(convertKeysToSnakeCase($data));
+
+            return $task->fresh();
+        });
+    }
+
+    public function getTaskById(string $id): Task {
+        $task = $this->taskModel->find($id);
+
+        throw_if(!$task, NotFoundException::class, 'Task');
+
+        return $task;
     }
 }

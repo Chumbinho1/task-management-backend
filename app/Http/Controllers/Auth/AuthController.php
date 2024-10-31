6ed12fdcb508d03f595faa8f944a60b3c43c6828
Chumbinho1 <vitorpereira041019@gmail.com>
2024-10-31 01:47:29 -0300

    update composer lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -14,14 +14,11 @@ class AuthController extends Controller
 {
     public function __construct(
         protected AuthService $authService
-    )
-    {
-    }
+    ) {}
 
     public function signup(
         AuthSignupRequest $request
-    ): JsonResponse
-    {
+    ): JsonResponse {
         try {
             $this->authService->signup($request->validated());
 
@@ -33,8 +30,7 @@ class AuthController extends Controller
 
     public function signin(
         AuthSigninRequest $request
-    ): JsonResponse
-    {
+    ): JsonResponse {
         try {
             return ok(SigninResource::make($this->authService->signin($request->validated())));
         } catch (InvalidCredentialsException $e) {
@@ -48,6 +44,7 @@ class AuthController extends Controller
     {
         try {
             $this->authService->signout();
+
             return ok(['message' => 'Signed out successfully!']);
         } catch (\Exception $e) {
             return internalServerError($e);

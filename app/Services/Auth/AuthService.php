2315b9a179dcaa127f6274267fe64acd1db04a81
Chumbinho1 <vitorpereira041019@gmail.com>
2024-10-31 01:44:25 -0300

    run pint and update composer lock

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -10,9 +10,7 @@ class AuthService
 {
     public function __construct(
         protected User $userModel
-    )
-    {
-    }
+    ) {}
 
     public function signup(array $data): User
     {
@@ -23,7 +21,7 @@ class AuthService
     {
         $user = $this->userModel->where('email', $data['email'])->first();
 
-        throw_if(!$user || !password_verify($data['password'], $user->password), InvalidCredentialsException::class, 'Invalid credentials.');
+        throw_if(! $user || ! password_verify($data['password'], $user->password), InvalidCredentialsException::class, 'Invalid credentials.');
 
         return $user->createToken('authToken');
     }
